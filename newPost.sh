d3c8b55c0e4ffe87a72c38991ed74ea0f7f808ae
Michał Sidor (Michcioperz) <public+git@meekchopp.es>
2018-01-20 10:36:58 +0100

    editor fallback for me

--- newPost.sh
+++ newPost.sh
@@ -3,5 +3,5 @@ num=1
 while [[ -f "posts/${num}.markdown" ]]; do
   (( num += 1 ))
 done
-$EDITOR "posts/${num}.markdown"
+${EDITOR:-vim} "posts/${num}.markdown"
 echo "Remember to commit if you got something done"
